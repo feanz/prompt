8f979f55a486d9954c0b683fe51181bf1efc2c72
Richard Forrest <richard.a.forrest@gmail.com>
2021-10-16 14:03:41 +0700

    Update terminal settings file

--- Setup.ps1
+++ Setup.ps1
@@ -13,3 +13,7 @@ winget install Github.cli --source winget
 Install-Module PSReadline -RequiredVersion 2.2.0-beta1 -AllowPrerelease -Repository PSGallery
 Install-Module Terminal-Icons
 Install-Module z
+
+# 1. Update the terminal settings Json file
+# 2. Copy the powershell profile file into you Documents Powershell folder 
+# 3. Copy the gitconfig file and alias file into your home directory
