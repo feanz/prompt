2ffdad7a1fd718249d6e29300bcbad06e43c1975
Richard Forrest <richard.a.forrest@gmail.com>
2022-02-11 10:25:22 +0700

    Clean up setup script and readme

--- Setup.ps1
+++ Setup.ps1
@@ -1,12 +1,14 @@
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
+winget install --id Git.Git -e --source winget
+winget install --id Microsoft.Powershell --source winget
 winget install JanDeDobbeleer.OhMyPosh
 winget install vim.vim
 winget install KDE.KDiff3
 winget install Github.cli --source winget
 choco install bat
 
-Install-Module PSReadline -RequiredVersion 2.2.0-beta1 -AllowPrerelease -Repository PSGallery
+Install-Module PSReadline 
 Install-Module Terminal-Icons
 Install-Module z
 
