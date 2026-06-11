23e149aff0dee913d80f99109591d6e0cbfa4d47
Richard Forrest <feanz@users.noreply.github.com>
2026-06-11 14:31:08 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,5 @@
 oh-my-posh init pwsh | Invoke-Expression
 
-function yolo {
-  param([string]$worktree = "")
-  if ($worktree) {
-    quell claude -- --dangerously-skip-permissions -w $worktree
-  } else {
-    quell claude -- --dangerously-skip-permissions
-  }
-}
-
 Import-Module Terminal-Icons
 Import-Module z
 
